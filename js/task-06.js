8663d2caa8aa89fcaff3cffeee0ae74f44b253f4
Elnara000 <110972810+Elnara000@users.noreply.github.com>
2023-03-04 12:29:28 +0200

    adds remarks

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,11 @@ const input = document.querySelector('#validation-input');
 
 input.addEventListener('blur', onInputBlur);
 
+//не получается( при исправлении с неправильного на нужное количество 
+// символов цвет не менятся. Удаление класса также не помогает. только перезагрузкой
 function onInputBlur (evt) {
-    if (evt.currentTarget.dataset.length == evt.currentTarget.value.length) {
-        input.classList.add('valid')
+    if (evt.currentTarget.dataset.length != evt.currentTarget.value.length) {        
+        input.classList.add('invalid')   
     }
-    input.classList.add('invalid')
+    input.classList.add('valid');
 }
